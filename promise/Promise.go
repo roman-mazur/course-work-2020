f8cc56ce4349efd70803f6935c586a0570e2fe94
stanislav7766 <stanislavtihonov5@gmail.com>
2020-05-27 20:02:59 +0300

    Implemented Then method

--- promise/Promise.go
+++ promise/Promise.go
@@ -1,6 +1,23 @@
 package promise
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+func checkOnError(value interface{}) error {
+	if err, ok := value.(error); ok && err != nil {
+		return err
+	}
+	return nil
+}
+func checkOnChan(value interface{}) chan interface{} {
+	if ch, ok := value.(chan interface{}); ok && ch != nil {
+		return ch
+	}
+	return nil
+}
 
 func (p *Promise) fulfill(result interface{}) {
 	p.state = fulfilled
@@ -11,6 +28,9 @@ func (p *Promise) reject(err error) {
 	p.state = rejected
 	p.err = err
 	p.resolved = true
+}
+func (p *Promise) resetState() {
+	p.state = pending
 }
 
 func NewPromise(executor Executor) *Promise {
@@ -40,4 +60,89 @@ func Reject(result string) *Promise {
 	return NewPromise(func(resolve Resolver, reject Rejecter) {
 		reject(errors.New(result))
 	})
+}
+
+func doResolve(resolve Resolver, reject Rejecter, cb func() interface{}, done chan bool) {
+	go func() {
+		defer func() {
+			done <- true
+			if err := recover(); err != nil {
+				reject(errors.New(fmt.Sprint(err)))
+			}
+			close(done)
+		}()
+		response := cb()
+		if p, isPromise := response.(*Promise); isPromise {
+			response = await(p)
+			if ch := checkOnChan(response); ch != nil {
+				response = <-ch
+			}
+		}
+		if err := checkOnError(response); err != nil {
+			reject(err)
+		} else {
+			resolve(response)
+		}
+	}()
+}
+
+func (p *Promise) Then(onFulfilled OnFulfilled, onRejected OnRejected) *Promise {
+	if p, ok := waitResolves([]*Promise{p}, 1, 1<<63-1); !ok && p != nil {
+		return nil
+	}
+	done := make(chan bool, 1)
+	next := NewPromise(func(resolve Resolver, reject Rejecter) {
+		doResolve(resolve, reject, func() interface{} {
+			if p.err != nil {
+				return onRejected(p.err)
+			} else {
+				return onFulfilled(p.value)
+			}
+		}, done)
+	})
+	<-done
+	next.resetState()
+	return next
+}
+func await(p *Promise) chan interface{} {
+	done := make(chan interface{}, 1)
+	go func() {
+		defer close(done)
+		p.Then(func(data interface{}) interface{} {
+			if pr, isPromise := data.(*Promise); isPromise {
+				done <- await(pr)
+			} else {
+				if ch := checkOnChan(data); ch != nil {
+					done <- <-ch
+				} else {
+					done <- data
+				}
+			}
+			return nil
+		}, func(err error) interface{} {
+			done <- err
+			return nil
+		})
+	}()
+	return done
+}
+func waitResolves(promises []*Promise, waitCount int, timeout time.Duration) (*Promise, bool) {
+	var pr *Promise
+	start := time.Now()
+	for i := 0; i < waitCount; i = 0 {
+		if time.Since(start) > timeout {
+			return pr, false
+		}
+		for _, p := range promises {
+			if !p.resolved {
+				continue
+			}
+			pr = p
+			i++
+		}
+		if i == waitCount {
+			break
+		}
+	}
+	return pr, true
 }
--- promise/Types.go
+++ promise/Types.go
@@ -17,3 +17,5 @@ type Promise struct {
 type Executor func(resolve Resolver, reject Rejecter)
 type Resolver func(value interface{})
 type Rejecter func(err error)
+type OnFulfilled func(data interface{}) interface{}
+type OnRejected func(err error) interface{}
